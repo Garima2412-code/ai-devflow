09fc2b9e29d933e69734cf30f6ff2594e7773991
Garima <ranagarima2412@gmail.com>
2026-09-12 22:07:31 +0530

    Added user model and auth routes

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+require ('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./src/config/db.js');
+const authRoutes = require('./src/routes/authRoute.js');
+
+const app = express();
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send("AI devflow Api is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} `);
+});
